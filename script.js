74fad78e9ce98f2622c654a18ef9379248d3d588
Josué <joheca1998@outlook.com>
2020-04-06 23:28:43 -0600

    Se creo la funcion para generar la secuencia de colores

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ const verde = document.getElementById('verde')
 class Juego {
   constructor() {
     this.inicializar()
+    this.generarSecuencia()
   }
 
   inicializar() {
     document.getElementById('btnEmpezar').classList.add('hideBtn')
+  }
+
+  generarSecuencia(){
+      this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
+      this.nivel = 1
+      this.colores = {
+          celeste,
+          violeta, 
+          naranja, 
+          verde
+      }
   }
 }
 
