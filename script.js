89ae3ffbee2a405137f2c7bdd9613cff0ed71b1c
Josué <joheca1998@outlook.com>
2020-04-06 23:48:05 -0600

    Se agrego la iluminación de la secuencia de colores

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Juego {
   constructor() {
     this.inicializar()
     this.generarSecuencia()
+    this.siguienteNivel()
   }
 
   inicializar() {
@@ -22,6 +23,39 @@ class Juego {
           naranja, 
           verde
       }
+  }
+
+  siguientenivel(){
+      this.iluminarSecuencia()
+  }
+
+  transformarNumeroAColor(num){
+      switch(numero){
+          case 0: 
+            return'celeste'
+          case 1: 
+            return 'violeta'
+          case 2:
+            return 'naranja'
+          case 3: 
+            return 'verde'
+      }
+  }
+
+  iluminarSecuencia(){
+      for(let i=0; i < this.nivel;i++){
+        const color = this.transformarNumeroAColor(this.secuencia[i])
+        setTimeout(()0> this.iluminarColor(color), 1000*i)
+      }
+  }
+
+  iluminarColor(color){
+      this.colores[color].classList.add('light')
+      setTimeout(() => this.apagarColor(color), 350)
+  }
+
+  apagarColor(color){
+      this.colores[color].classList.remove('light')
   }
 }
 
