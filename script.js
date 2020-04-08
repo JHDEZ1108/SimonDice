e118f52e722a6678687a123d138e946b15096c44
Josué <joheca1998@outlook.com>
2020-04-08 14:51:46 -0600

    Agregando estados finales del juego

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
+const ULTIMO_NIVEL = 10
+const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
   constructor() {
     this.inicializar()
     this.generarSecuencia()
     this.siguienteNivel()
-  this.iluminarColor = this.iluminarColor.bind(this)
-  this.apagarColor = this.apagarColor.bind(this)
-  this.elegirColor = this.elegirColor.bind(this)
+    this.inicializar = this.inicializar.bind(this)
   }
-
+  
   inicializar() {
-    document.getElementById('btnEmpezar').classList.add('hideBtn')
-    this.nivel = 7
+    this.iluminarColor = this.iluminarColor.bind(this)
+    this.apagarColor = this.apagarColor.bind(this)
+    this.elegirColor = this.elegirColor.bind(this)
+    this.transformarColorANumero = this.transformarColorANumero.bind(this)
+    this.transformarNumeroAColor = this.transformarNumeroAColor.bind(this)
+    this.siguienteNivel = this.siguienteNivel.bind(this)
+    this.toggleBtnEmpezar()
+    this.nivel = 1
+  }
+  
+  toggleBtnEmpezar(){    
+    if(document.getElementById('btnEmpezar').classList.contains('hideBtn')){
+      document.getElementById('btnEmpezar').classList.remove('hideBtn')
+    }else{
+      document.getElementById('btnEmpezar').classList.add('hideBtn')
+    }
   }
 
   generarSecuencia(){
-      this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
+      this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random()*4))
   }
 
   siguienteNivel(){
+      this.subnivel = 0
       this.iluminarSecuencia()
-      this.agregarEventosClic()
+      this.agregarEventosClick()
   }
 
   transformarNumeroAColor(num){
@@ -35,6 +50,19 @@ class Juego {
       }
   }
 
+  transformarColorANumero(dataColor){
+    switch(dataColor){
+        case 'celeste': 
+          return 0
+        case 'violeta': 
+          return 1
+        case 'naranja':
+          return 2
+        case 'verde': 
+          return 3
+    }
+}
+  
   iluminarSecuencia(){
       for(let i=0; i < this.nivel; i++){
         const color = this.transformarNumeroAColor(this.secuencia[i])
@@ -51,18 +79,57 @@ class Juego {
       color.classList.remove('light')
   }
 
-  agregarEventosClic(){
+  agregarEventosClick(){
     document.getElementById('celeste').addEventListener('click', this.elegirColor)
     document.getElementById('violeta').addEventListener('click', this.elegirColor)
     document.getElementById('naranja').addEventListener('click', this.elegirColor)
     document.getElementById('verde').addEventListener('click', this.elegirColor)
   }
 
+  eliminarEventosClilck(){
+    document.getElementById('celeste').removeEventListener('click', this.elegirColor)
+    document.getElementById('violeta').removeEventListener('click', this.elegirColor)
+    document.getElementById('naranja').removeEventListener('click', this.elegirColor)
+    document.getElementById('verde').removeEventListener('click', this.elegirColor)
+  }
+
   elegirColor(ev){
-    console.log(ev)
+    const nombreColor = ev.target.dataset.color
+    const numeroColor = this.transformarColorANumero(nombreColor)
+    this.iluminarColor(this.transformarNumeroAColor(numeroColor))
+
+    if(numeroColor === this.secuencia[this.subnivel]){
+      this.subnivel++
+      if(this.subnivel == this.nivel){
+        this.nivel++
+        this.eliminarEventosClilck()
+        if(this.nivel===(ULTIMO_NIVEL + 1)){
+          this.ganoElJuego()
+        }else{
+          setTimeout(this.siguienteNivel, 1500)
+        }
+      }
+    }else{
+      this.perdioElJuego()
+    }
+  }
+  
+  ganoElJuego(){
+    swal('HEY!', 'Felicitaciones, ganaste el juego!', 'success')
+    .then(()=>{
+      this.eliminarEventosClilck()
+      this.inicializar()
+    })
+  }
+  
+  perdioElJuego(){
+    swal('HEY!', 'Lo siento, perdiste:(', 'error')
+    .then(()=>{
+      this.eliminarEventosClilck()
+      this.inicializar()
+    })
   }
 }
-
 function empezarJuego() {
   window.juego = new Juego()
 }
